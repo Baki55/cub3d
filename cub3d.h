e0df3019d51178eb75b52a8adf35a3d893838956
Badreldine Khatib <bkhatib@fu-c4-p5.s19.be>
2023-02-27 11:30:46 +0100

    added KEY move define + passed the norminette

--- cub3d.h
+++ cub3d.h
@@ -3,24 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: blaurent <blaurent@student.s19.be>         +#+  +:+       +#+        */
+/*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 19:22:44 by bkhatib           #+#    #+#             */
-/*   Updated: 2023/02/23 18:19:02 by blaurent         ###   ########.fr       */
+/*   Updated: 2023/02/27 11:29:45 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
-#define CUB3D_H
+# define CUB3D_H
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <string.h>
-#include <math.h>
-#include <sys/errno.h>
-#include "mlx.h"
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <string.h>
+# include <math.h>
+# include <sys/errno.h>
+# include "mlx.h"
+
+/* Windows */
+// #define KEY_UP 122
+// #define KEY_DOWN 115
+// #define KEY_RIGHT 100
+// #define KEY_LEFT 113
+// #define KEY_ESC 65307
+/* Mac */
+# define KEY_UP 13
+# define KEY_DOWN 1
+# define KEY_RIGHT 2
+# define KEY_LEFT 0
+# define KEY_ESC 53
 
 typedef struct s_color
 {
@@ -107,19 +120,21 @@ typedef struct s_program
 	int			screen_h;
 }				t_program;
 
-
 /* Init functions */
 void	init_mlx_win_pos(t_program *p);
 void	init_color(t_color *color);
 void	init_map(t_map *map);
 void	init_game(t_program *game);
+
 /* movements functions */
 int		move(int key, t_program *p);
+
 /* raycast functions */
 void	update_camera_raydir_delatdist(t_program *p);
 void	get_step_and_side_dist(t_program *p);
 void	search_wall(t_program *p);
 void	calculate_wall_size(t_program *p);
+
 /* rendering functions */
 void	my_mlx_pixel_put(t_img *img, int x, int y, int color);
 void	draw_vertical_line(t_program *p, int ceilling_color, int floor_color);
@@ -127,6 +142,7 @@ int		rgb_to_int(t_color color);
 t_img	get_tex(t_program *game_ptr);
 void	init_tex(t_program *game_ptr, t_img tex);
 void	put_tex(t_program *game_ptr, t_img tex);
+
 /* Parser functions */
 void	parser(t_program *ptr);
 int		check_extension(char *src, char *to_find);
